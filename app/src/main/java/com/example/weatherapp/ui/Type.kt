c157b2a2b03bacbf27a619f15a578108d8f6c398
Yusuf <yusuf.abdiqadir2@gmail.com>
2023-08-08 02:09:20 -0500

    Assignment4

---
+++ app/src/main/java/com/example/weatherapp/ui/Type.kt
@@ -0,0 +1,48 @@
+package com.example.weatherapp.ui
+
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+        fontSize = 64.sp,
+        lineHeight = 68.sp,
+        letterSpacing = 1.sp
+    ),
+
+    titleSmall = TextStyle(
+        fontFamily = FontFamily.Cursive,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 1.0.sp,
+    ),
+
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.SemiBold,
+        textAlign = TextAlign.Center,
+        fontSize = 28.sp,
+        shadow = Shadow(color = PurpleGrey80, offset = Offset(x = 5F, y = 5F), blurRadius = 5F)
+
+    ),
+
+    bodySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp
+    )
+
+)
